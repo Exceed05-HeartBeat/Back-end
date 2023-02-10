8777e2ec50c3801ed6d27bb5cd21d0b71747a180
JiratchayaPhinyodom <jiratchaya.phiny@ku.th>
2023-02-10 09:43:07 +0700

    Add heart rate model

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import os
 from pymongo import MongoClient
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+from typing import Optional
 load_dotenv(".env")
 
 USER = os.getenv("username")
@@ -9,4 +12,12 @@ client = MongoClient(f"mongodb://{USER}:{PASSWORD}@mongo.exceed19.online:8443/?a
 
 db = client["exceed05"]
 hb_collection = db["heart-beat"]
+
+class HeartBeat(BaseModel):
+    name: Optional[str]
+    birth: Optional[datetime]
+    current_heartrate: Optional[int]
+    heartrate_excercise: Optional[int]
+    normal_heartrate: Optional[int]
+    is_on : bool
 
