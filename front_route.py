39ce40d96babc58df219afc19ab89b73aae4bc82
JiratchayaPhinyodom <jiratchaya.phiny@ku.th>
2023-02-10 13:59:27 +0700

    add path

--- front_route.py
+++ front_route.py
@@ -6,6 +6,30 @@ import datetime
 
 router = APIRouter(prefix="/front")
 
+def get_field_from_hb_collection(k: list):
+    ret = {}
+    data = hb_collection.find_one({}, {"_id": 0});
+    if not data:
+        print("Not Ok")
+    for r in k:
+        ret[r] = data[r]
+    return ret
+
+@router.post("/data")
+def front_post_data(heartrate: HeartRate = Body()):
+    hb_collection.update_one({}, {"$set" :{"name":heartrate.name, "birth":heartrate.birth}})
+    return {"name":heartrate.name, "birth":heartrate.birth}
+
+def calculate_maxrate():
+    hr = hb_collection.find_one({}, {"_id": False})
+    birth = hr["birth"]
+    year = datetime.datetime.strptime(birth, '%Y-%m-%d').year
+    current_year = datetime.datetime.now().year
+    age = current_year - year
+    max_rate = 207 - (age*0.7)
+    return max_rate
+
+
 @router.get("/get_status")
 def front_get_status():
     return {"status": 1}
@@ -20,40 +44,30 @@ def front_get_mode():
     md = hb_collection.find_one({}, {"_id": False})
     return {"current_heartrate": md["current_heartrate"]}
 
-# @router.get("/get_normal_heartrate")
-# def front_get_normal_heartrate():
-#     hr = hb_collection.find_one({}, {"_id": False})
-#     return hr["normal_heartrate"]
+@router.get("/status_excersice_mode")
+def hard_get_status():
+    hard_rate = get_field_from_hb_collection(["current_heartrate"])["current_heartrate"]
+    max_rate = calculate_maxrate()
+    # Mock status
+    if (hard_rate > max_rate*0.8): #red
+        status = 2
+    elif (hard_rate > max_rate*0.7 and hard_rate < max_rate*0.8): #yellow
+        status = 1
+    elif (hard_rate < max_rate*0.7): #less than 70% green
+        status = 0
+    return {"status": status}
 
-# @router.get("/get_excercise_heartrate")
-# def front_get_normal_heartrate():
-#     hr = hb_collection.find_one({}, {"_id": False})
-#     return hr["excercise_heartrate"]
-
-def calculate_maxrate():
-    hr = hb_collection.find_one({}, {"_id": False})
-    birth = hr["birth"]
-    year = datetime.datetime.strptime(birth, '%Y-%m-%d').year
-    current_year = datetime.datetime.now().year
-    age = current_year - year
-    max_rate = 207 - (age*0.7)
-    return max_rate
-
-#need to insert insteed of update
-@router.post("/data")
-def front_post_data(heartrate: HeartRate = Body()):
-    hb_collection.update_one({}, {"$set" :{"name":heartrate.name, "birth":heartrate.birth}})
-    # print(datetime.datetime.strptime(heartrate.birth, '%Y-%m-%d').year)
-    return {"name":heartrate.name, "birth":heartrate.birth}
-    # print(heartrate.name)
-    # print(heartrate.birth)
-
-# def calculate_maxrate():
-#     hr = hb_collection.find_one({}, {"_id": False})
-#     birth = hr["birth"]
-#     year = datetime.datetime.strptime(birth, '%Y-%m-%d').year
-#     return year
-# print(calculate_maxrate())
+@router.get("/status_normal_mode")
+def hard_get_status():
+    hard_rate = get_field_from_hb_collection(["current_heartrate"])["current_heartrate"]
+    # Mock status
+    if (hard_rate > 110): #red
+        status = 2
+    elif (hard_rate > 100 and hard_rate < 110): #yellow
+        status = 1
+    elif (hard_rate < 100): #less than 70% green
+        status = 0
+    return {"status": status}
 
 
 
